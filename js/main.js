df586bdf69be831393ca8caf3b6840488e6b4e16
Facundo Tejeda <facundotejeda18@gmail.com>
2022-05-02 20:07:43 -0300

    Incorporacion del Producto Tarjeta de Credito

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ let clave = "claves"
 let token = "token de seguridad"
 let transferencia = "transferencia de fondos"
 let pagos ="pagos de servicios"
+let planV ="plan v TC"
+let interesesTC = "intereses generados de TC"
+let blanqueo = "blanqueo de pin TC"
+let dolaresTC= "pago de dolares TC"
 let names = prompt("Bienvenido a Banco Random, por favor ingrese su nombre y apellido: ")
 let age = prompt("Por favor ingrese su edad:")
 let sueldo = prompt("cobra su sueldo en Banco macro?: ingrese SI/NO")
@@ -155,12 +159,12 @@ if(consultaAccion(help)==transferencia){
             alert("Sin tarjeta de Debito no transferir, favor de acercarse a sucursal.")
         }}
 }
-////Desarrollo de Banca Digital PAGOS
+//Desarrollo de Banca Digital PAGOS
 if(consultaAccion(help)== pagos){
     helpPay = prompt("Favor de determinar: Pagos de Servicios(1), VEP(2), Pagos por Cajero(3): ")
     if(helpPay != 3){
         if(td.toLowerCase()== "si"){
-            let tokenTransf = prompt("Posee Token Activo?: Si(1), No (2)")
+            let tokenTransf = prompt("Posee Token Activo?: Si, No")
             if(tokenTransf.toLowerCase() == "si"){
                 if(helpPay == 1){
                 alert("Ingrese a Banca Internet en la opcion pagos de servicio, nuevo pago, selecciones Rubro, seleccione sub-rubro, ingrese codigo de identificacion")
@@ -179,12 +183,96 @@ if(consultaAccion(help)== pagos){
     }}}
 
 //Tarjeta de credito
+indexTC=[1,2,3,4]
 
+let helpTC = "que consulta tiene sobre el producto Tarjeta de Credito?"
 if (consultaProducto(product)==product1){
     alert("Excelente, te instruiremos en el producto" + " " + product1.toUpperCase());
-    help = prompt("Ingrese sobre que items tiene inconvenientes: Plan V(1), Interes por Falta de Pago(2), Blanqueo de Pin(3), Compra en Dolares(4)");
-} 
+    let helpTC = Number(prompt("Ingrese sobre que items tiene inconvenientes: Plan V(1), Interes por Falta de Pago(2), Blanqueo de Pin(3), Pago saldo en Dolares(4)"));
+    console.log(indexTC.includes(helpTC))
+    while (indexTC.includes(helpTC)==false){
+    console.log("Accion no disponible, favor ingrese otro");
+    helpTC = Number(prompt("Favor de indicar otra operacion: Plan V(1), Interes por Falta de Pago(2), Blanqueo de Pin(3), Pago saldo en Dolares(4)"));
+    }
 
+//Desarrollo de PLAN V
+
+if(helpTC == indexTC[0]){
+    let helpPlanV= prompt(names+ " seleccionaste la opcion Plan V, cual es tu consulta?: Como acceder?(1), En que consiste la financiacion?(2), Calculo de interes? (3)")
+    if(helpPlanV==1){
+        alert("La opcion de Plan V la otorga visa, podras ver las opciones en tu resumen de TC, debera pagar el minimo del resumen vigente, luego llamar al telefono indicado 0800-222-CUOTAS. La opcion no aparece en tu resumen deberas realizar la consulta en VISAHOME")
+    }
+    else if(helpPlanV==2){
+        alert("La financiacion a la que usted esta accediendo solo abarca el resumen actual, es decir, usted coutificara el saldo restante al pago minimo por el plazo estipulado")
+        alert("CUIDADO: las cuotas faltantes de las compras que figuran en el resumen SEGUIRAN llegando en los proximos resumenes.")
+    }
+    else if(helpPlanV==3){
+        alert("calcularemos la cuota que abonaria segun el plazo y la deuda total")
+        let deuda = Number(prompt("ingrese Saldo Actual del Resumen: "))
+        let pagoMinimo = Number(prompt("ingrese Pago Minimo Actual del Resumen: "))
+        let plazo = Number(prompt("ingrese Plazo de financiacion: "))
+        let tasa = ((0.51/plazo))
+        console.log(tasa)
+        let saldoAdeudado = (deuda-pagoMinimo);
+        let numerador = saldoAdeudado*tasa*((1+tasa)**plazo)
+        console.log(numerador+" numerador")
+        let denominador= (((1+tasa)**plazo)-1)
+        console.log(denominador+" denominador")
+        let cuota = numerador / denominador
+        cuota = cuota.toFixed(2)
+        console.log(`usted abonara ${plazo} cuotas de ${cuota}`)
+        alert(`usted abonara ${plazo} cuotas de ${cuota} devolviendo en total ${cuota*plazo}`)
+    }
+}
+
+//Desarrollo de INTERESES DE TC
+if(helpTC == indexTC[1]){
+    alert("En esta seccion calcularemos los intereses punitorios(falta de pago) de tu resumen de TC")
+    let sa = prompt("Ingrese el Saldo Actual del resumen: ")
+    let pm = prompt("Ingrese el Pago Minimo del resumen: ")
+    let pago= prompt("Ingrese pago realizado: ")
+    let fechaPago = prompt("Ingrese fecha de pago(solo el dia asumimos que fue en el mes de abril): ")
+    let fechaVenc = prompt("Ingrese fecha de Vencimiento del Resumen (solo el dia asumimos que fue en el mes de abril):")
+    let diasAtraso= fechaPago-fechaVenc
+    let tna = 30.30
+    if((pago>=pm) && (fechaPago<=fechaVenc)){
+        alert("No corresponde abonar Interes Punitorio dado que abono mas o igual de pago minimo dentro de la fecha de vencimiento")
+    } else{
+        let saldoPunitorio = pm - pago
+        let intPunitorio = (saldoPunitorio*(diasAtraso)*tna)/365
+        alert(`usted abonara ${intPunitorio.toFixed(2)} correspondiente a intereses punitorios`)
+    }
+
+}
+
+//Desarrollo de BLANQUEO DE PIN TC
+if(helpTC == indexTC[2]){
+    let helpPinTC = prompt("Posee clave Telefonica?: SI(1), NO(2)")
+    if(helpPinTC == 1){
+        alert("Debe comunicarse al 0810-555-XXXX e ingresar a la opcion Operar con sus Productos y allí solicitar el blanqueo")
+    }else{
+        alert("Deberá ingresar a VISAHOME y solicitar el Blanqueo de Pin de la Tarjeta de credito, caso contrario, acercarse a la sucursal mas cercana")
+    }
+}
+
+//Desarrollo de PAGO DE SALDO EN DOLARES
+if(helpTC == indexTC[3]){
+    helpDolarTC= prompt("Favor de indicar cual es su consulta: Cotización Dolar (1), Saldo en Dolares Positivo y Pesos en Negativo (2), Excencion de Impuestos(3): ")
+    if(helpDolarTC == 1){
+        let pagoDolar = prompt("Abonara en: Pesos(1), Dolar(2)")
+        if(pagoDolar == 1){
+            alert("La cotización del Dolar que ustede debe abonar es la oficial, dado que los impuestos son cobrados en el resumen en el que los dolares son consumidos") 
+        }else{
+            alert("Al abonar en dolares, los impuestos seran descontados al cierre del resumen, por lo que debera abonar el saldo en dolares MAS el saldo en pesos menos los impuestos.")
+        }
+    }else if(helpDolarTC==2){
+        alert("El saldo negativo en pesos significa que usted posee un saldo a Favor, el mismo se ajustara con el saldo a dolares positivo que posee hoy en la Tarjeta de Credito, figura de esta manera dado que la marca realiza la compenzacion al cierre del nuevo resumen.")
+    }else if(helpDolarTC==3){
+        alert("El unico caso en el que los impuestos no deben ser cobrados, es en aquellos productos que el BCRA a determinado, debera verificar en https://www.afip.gob.ar/impuesto-pais/operaciones-y-sujetos/no-alcanzados.asp y en caso de corresponder, Cargar un Reclamo")
+    }
+        
+}
+}
 //Tarjeta de Debito
 if (consultaProducto(product)==product3){
     alert("Excelente, te instruiremos en el producto" + " " + product3.toUpperCase());
